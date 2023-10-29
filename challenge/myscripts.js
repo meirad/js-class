67de68d61ae1d609b34613dd1ea2450fad0cd35b
meira ramirz <meira.dobkin@gmail.com>
2023-10-29 15:13:18 +0200

    m

--- challenge/myscripts.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('keydown', (e) => {
-    if (e.key == "b" && e.ctrlKey) {
-        const element = document.querySelector('.blur'); // Replace '
-        alert('you find the secret key binding');
-        element.classList.remove('blur');
-        document.getElementById('title').style.display = "none";
-    }
-});
